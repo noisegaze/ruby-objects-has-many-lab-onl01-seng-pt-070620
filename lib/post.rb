2c527b1e5d7eb462c3db9e4944d8a1d443af2aca
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-05 19:37:59 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Post 
+  
+  attr_accessor :title, :author 
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    save
+  end 
+  
+  def save 
+    @@all << self 
+  end  
+  
+  def self.all
+    @@all
+  end   
+  
+  def author_name
+   if self.author 
+     author.name
+    else 
+      nil 
+    end
+  end  
+    
+  
+  
+  
+  
+  
+  
+end
